76a876b09d00323881b7bb8bb7d4da4bda8a57d7
KGLubabah <khairis_ghifariy_27rpl@student.smktelkom-mlg.sch.id>
2019-04-09 09:35:32 +0700

    commit pertama

---
+++ Login/form-login.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<form action="login.php" method="post">
+			<h5>Username : <input type="text" name="username"></h5>
+			<h5>Password : <input type="password" name="password"></h5>
+	
+			<input type="submit" name="btnSimpan"value="Simpan">
+		</form>
+	</body>
+</html>
---
+++ Matakuliah/form-create.php
@@ -0,0 +1,34 @@
+<?php
+
+	include '../connect.php';
+
+	$query = "SELECT id_dosen, nama_dosen FROM dosen";
+	$result = mysqli_query($connect, $query);
+	
+?>
+
+<!DOCTYPE html>
+<html>
+	<body>
+		<h2>Tambah Data Matakuliah</h2>
+		<form action="create.php" method="post">
+			<h5>Kode : <input type="text" name="kode"></h5>
+			<h5>Matakuliah : <input type="text" name="nama_matkul"></h5>
+			<h5>SKS : <input type="text" name="sks"></h5>
+			<h5>Semester : <input type="text" name="semester"></h5>
+			Dosen Pengajar :  <select name="id_dosen" id="nama_dosen">
+			<option value="NULL">-- Pilih Salah satu --</option>
+				<?php
+				while ($data = mysqli_fetch_assoc($result)){
+				?>
+					<option value="<?php echo $data['id_dosen']; ?>">
+					<?php echo $data['nama_dosen']; ?>
+					</option>
+				<?php
+				}
+			?>
+			</select>
+			<input type="submit" name="submit"value="Simpan">
+		</form>
+	</body>
+</html>
